7a8a8a6aaa8ab298de2b7bc9d0c8cfa682409e4a
Kathaleeya Anakjin <60010123@kmitl.ac.th>
2020-02-07 02:35:35 +0700

    check at login in navbar on home screen

--- src/views/homeScreen/homeScreen.vue
+++ src/views/homeScreen/homeScreen.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="homeScreen">
     <div class="navBar">
-      <div>
-        หน้าหลัก
+      <div class="homeBox">
+        <router-link to="/">หน้าหลัก</router-link>
       </div>
-      <div class="loginButton">
-        เข้าสู่ระบบ
+      <div class="loginBox" v-if="!token">
+        <router-link to="/login">เข้าสู่ระบบ</router-link>
+      </div>
+      <div class="" v-else>
+        {{ username }}
+        <router-link to="mybooking">รายชื่อโรงแรมที่จอง</router-link>
+        <div v-on:click="logout()">
+          ออกจากระบบ
+        </div>
       </div>
     </div>
     <router-view></router-view>
@@ -15,6 +22,22 @@
 <script>
 export default {
   name: "homeScreen",
+  data() {
+    return {
+      token: "",
+      username: ""
+    };
+  },
+  mounted() {
+    this.token = localStorage.token;
+    this.username = localStorage.username;
+  },
+  methods: {
+    logout() {
+      localStorage.clear();
+      location.reload();
+    }
+  },
   components: {},
   props: {}
 };
@@ -32,9 +55,14 @@ export default {
   bottom: 0;
   display: inline-flex;
 }
-.loginButton {
-  width: 50%;
+.homeBox {
+  width: 70%;
+  padding: 2%;
+}
+.loginBox {
+  width: 30%;
   right: 0;
   text-align: right;
+  padding: 2%;
 }
 </style>
