d41f6a69619ddc78a1264fd884218e47c3118e9a
Kathaleeya Anakjin <60010123@kmitl.ac.th>
2020-02-06 23:43:33 +0700

    create booking list screen and component

--- src/views/bookingListScreen/bookingListScreen.vue
+++ src/views/bookingListScreen/bookingListScreen.vue
@@ -1,16 +1,68 @@
 <template>
   <div class="bookingListScreen">
-    BookingList
+    <div v-if="!isLoading">
+      <h2>Booking List</h2>
+      <div v-for="hotel in bookingList" :key="hotel">
+        <bookingBox :booking="hotel"></bookingBox>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-//import headerMessenger from '@/views/TypePeople/components/headerMessenger.vue'
+import bookingBox from "@/views/bookingListScreen/components/bookingBox.vue";
 
 export default {
   name: "bookingListScreen",
-  components: {},
+  data() {
+    return {
+      bookingList: [],
+      isLoading: true
+    };
+  },
+  mounted() {
+    this.fetchBooking();
+  },
+  methods: {
+    fetchBooking() {
+      fetch(
+        `https://4bfb9a7a-3df0-4222-a22f-d666eea1a18f.mock.pstmn.io/booking`,
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            Authorization: "Bearer " + localStorage.token
+          }
+        }
+      )
+        .then(response => {
+          if (response.status != 200) {
+            console.log("ERROR Status: " + response.status);
+          } else {
+            return response.json();
+          }
+        })
+        .then(response => {
+          this.bookingList = response.data;
+          this.isLoading = false;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  },
+  components: {
+    bookingBox
+  },
   props: {}
 };
 </script>
-<style scoped></style>>
+<style scoped>
+.bookingListScreen {
+  position: fixed;
+  top: 10vh;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}</style
+>>
---
+++ src/views/bookingListScreen/components/bookingBox.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="bookingBox">
+    <div>{{ hotelBooking.name }}</div>
+    <div>Room: {{ hotelBooking.rooms }}</div>
+    <div>Date: {{ hotelBooking.date }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "bookingBox",
+  data() {
+    return {
+      hotelBooking: this.booking
+    };
+  },
+  mounted() {},
+  methods: {},
+  props: {
+    booking: Object
+  }
+};
+</script>
+<style scoped>
+.bookingBox {
+  width: 90%;
+  height: 20%;
+  border: solid;
+  margin: auto;
+  margin-top: 1%;
+  padding: 1%;
+}</style
+>>
