7caebcc0e9f91d0ee7f38a39ce69ad6f11b71dc3
Kathaleeya Anakjin <60010123@kmitl.ac.th>
2020-02-07 23:44:48 +0700

    edit background position at register screen

--- src/views/registerScreen/registerScreen.vue
+++ src/views/registerScreen/registerScreen.vue
@@ -141,5 +141,9 @@ input {
 .registeButton:hover {
   box-shadow: 2px 5px 4px rgba(0, 0, 0, 0.25);
   background-color: #6abaff;
-}</style
->>
+}
+#backgroung{
+  left: 1200px;
+  top: 320px;
+}
+</style>
